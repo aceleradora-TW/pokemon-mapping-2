66fea8db5725fcbb766c233267d6cf9921ac7155
leonardo gomes <leonardogomespadilha@gmail.com>
2021-09-08 15:00:51 -0300

    @leonardogomesp @gustavo1x @ErickCardoso007 - Adiciona funçoes para encontrar dados na API

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ const encontrarVersao = (versaoRedBlue) => {
     return versaoRedBlue['version_group'].name === 'red-blue'
 }
 
-const encontrarStatus= (a, b) => {
-    a.stats.find(status => status.stat.name === b)
+const encontrarStatus= (pokemon, atributoDoMomento) => {
+    pokemon.stats.find(status => status.stat.name === atributoDoMomento)
 }
+
+
+
+/* attributes: {
+    hp: encontrarStatus(umAtributoQualquer, 'hp')
+    defesa: encontrarStatus(umAtributoQualquer, 'defense')
+}
+ */
