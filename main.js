30d940ca044408b5bfbc826be131c28602f9a34b
leonardo gomes <leonardogomespadilha@gmail.com>
2021-09-09 15:57:29 -0300

    @leonardogomesp @gustavo1x @erickcardoso007 - Inicia implementação da função imprimir

--- main.js
+++ main.js
@@ -8,8 +8,17 @@ const encontrarStatus= (pokemon, atributoDoMomento) => {
     pokemon.stats.find(status => status.stat.name === atributoDoMomento)
 }
 
+const imprimir = api.map(pokemon => ({
+    nome: pokemon.name,
+    tipo: pokemon.types.map(tipo => tipo.type.name),
+    habilidade: pokemon.abilities.map(habilidade => habilidade.ability.name),
+    atributos: {
+        ataque: encontrarStatus(pokemon, "attack"),
+        defesa: encontrarStatus(pokemon, "defense"),       
+    }
+}))
 
-
+console.log(imprimir)
 /* attributes: {
     hp: encontrarStatus(umAtributoQualquer, 'hp')
     defesa: encontrarStatus(umAtributoQualquer, 'defense')
