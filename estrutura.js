57ef6238b4612d1ea7261975e87295a017a5e3c1
Jorel Martins <bearshamis@gmail.com>
2021-09-10 11:00:39 -0300

    fazendo

--- estrutura.js
+++
@@ -1,6 +0,0 @@
-function primeiraLetraMaiuscula(palavra) {
-    const palavraSemEspaco = palavra.trim()
-    return palavraSemEspaco[0].toUpperCase() + palavraSemEspaco.slice(1).toLowerCase()
-}
-
-module.exports = primeiraLetraMaiuscula
--- main.js
+++ main.js
@@ -1,12 +1,55 @@
 const response = require("./api")
-const estr = require("./estrutura")
-console.log(response);
+
+const capitalize = (string) => {
+    // se a palavra nao for válida retornará null
+    if (!isValid(string)) return null
+  
+    // caso seja válida, vai separar as palavras por espaço e criar um array com elas
+    let words = string.split(" ")
+    // com esse array, utilizo o map para conseguir transformar a primeira inicial de cada palavra em maiuscula
+    return words.map(
+      (word) => `${word[0].toUpperCase()}${word.substring(1)}`
+    ).join(" ") //ao final, uso join para juntar o array novamente em uma palavra apenas
+  }
+
+function obterNumeros(pokemon) {return parseInt(pokemon)}
+const formatNumber = number => parseFloat(number.toFixed(1))
+
+
+const buscarnome = (lista) => {
+  
+    const buscandoNome = lista.find((response) => response === 'name' )
+    return buscandoNome
+
+    // const inventory = [
+    //     {name: 'apples', quantity: 2},
+    //     {name: 'bananas', quantity: 0},
+    //     {name: 'cerejas', quantity: 5}
+    // ];
+    
+    // function isCherries(fruit) {
+    //     return fruit.name === 'cerejas';
+    // }
+    
+    // console.log(inventory.find(isCherries));
+    // // { name: 'cerejas', quantity: 5 }
+
+
+    // const spaces = "   "
+    // return `${spaces} Lv ${capitalize(name.forms.name)} - ${obterNumeros(id.id)}`
+    
+  }
 
 
 
 
 
+const imprimir = () =>{
+console.log(buscarnome(response));
+    
+}
 
+imprimir()
 
 // const pokemon = {
 //     id: Number,
