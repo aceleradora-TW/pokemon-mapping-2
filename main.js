e4a0b0bbf2cd6705f0e5b530445f5819e35ef6ef
leonardo gomes <leonardogomespadilha@gmail.com>
2021-09-10 10:57:54 -0300

    @leonardogomesp @gustavo1x @erickcardoso007 - Implementa busca por machine e tutor

--- main.js
+++ main.js
@@ -8,14 +8,30 @@ const encontrarStatus= (pokemon, atributoDoMomento) => {
     pokemon.stats.find(status => status.stat.name === atributoDoMomento)
 }
 
+const movesSemMachineETutor = (move) => {
+    const acharVersao = move['version_group_details'].find(version => encontrarVersao(version))
+    if (!acharVersao) {
+        return false
+    }
+    return acharVersao['move_learn_method'].name !== 'tutor' || acharVersao['move_learn_method'].name !== 'machine'
+}
+
 const imprimir = api.map(pokemon => ({
     nome: pokemon.name,
     tipo: pokemon.types.map(tipo => tipo.type.name),
     habilidade: pokemon.abilities.map(habilidade => habilidade.ability.name),
     atributos: {
+        id: pokemon.id,
+        hp: encontrarStatus(pokemon, "hp"),
         ataque: encontrarStatus(pokemon, "attack"),
-        defesa: encontrarStatus(pokemon, "defense"),       
-    }
+        ataqueEspecial: encontrarStatus(pokemon, "special-attack"),
+        defesa: encontrarStatus(pokemon, "defense"), 
+        defesaEspecial: encontrarStatus(pokemon, "special-defense"),
+        velocidade: encontrarStatus(pokemon, "speed"),      
+    },
+     moves: pokemon.moves.filter(move => movesSemMachineETutor(move)).map(
+         
+     )
 }))
 
 console.log(imprimir)
