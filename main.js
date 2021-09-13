0041623c7d5c08fc906dc0df608c2772c7af306c
leonardo gomes <leonardogomespadilha@gmail.com>
2021-09-13 10:35:00 -0300

    @gustavo1x @erick007 @leonardogomesp Finaliza exercicio mapping

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const encontrarVersao = (versaoRedBlue) => {
     return versaoRedBlue['version_group'].name === 'red-blue'
 }
 
-const encontrarStatus= (pokemon, atributoDoMomento) => {
-    pokemon.stats.find(status => status.stat.name === atributoDoMomento)
+const encontrarStatus= (pokemon, atributoDoMomento) => {    
+    const status = pokemon.stats.find(status => status.stat.name === atributoDoMomento)
+    return status.base_stat
 }
 
 const movesSemMachineETutor = (move) => {
@@ -13,28 +14,33 @@ const movesSemMachineETutor = (move) => {
     if (!acharVersao) {
         return false
     }
-    return acharVersao['move_learn_method'].name !== 'tutor' || acharVersao['move_learn_method'].name !== 'machine'
+    return acharVersao['move_learn_method'].name !== 'tutor' && acharVersao['move_learn_method'].name !== 'machine'
 }
 
 const imprimir = api.map(pokemon => ({
-    nome: pokemon.name,
-    tipo: pokemon.types.map(tipo => tipo.type.name),
-    habilidade: pokemon.abilities.map(habilidade => habilidade.ability.name),
-    atributos: {
-        id: pokemon.id,
+    id: pokemon.id,
+    name: pokemon.name,
+    types: pokemon.types.map(tipo => tipo.type.name),
+    abilities: pokemon.abilities.map(habilidade => habilidade.ability.name),
+    attributes: {
         hp: encontrarStatus(pokemon, "hp"),
-        ataque: encontrarStatus(pokemon, "attack"),
-        ataqueEspecial: encontrarStatus(pokemon, "special-attack"),
-        defesa: encontrarStatus(pokemon, "defense"), 
-        defesaEspecial: encontrarStatus(pokemon, "special-defense"),
-        velocidade: encontrarStatus(pokemon, "speed"),      
+        attack: encontrarStatus(pokemon, "attack"),
+        specialAttack: encontrarStatus(pokemon, "special-attack"),
+        defense: encontrarStatus(pokemon, "defense"), 
+        specialDefense: encontrarStatus(pokemon, "special-defense"),
+        speed: encontrarStatus(pokemon, "speed"),      
     },
      moves: pokemon.moves.filter(move => movesSemMachineETutor(move)).map(
-         
+         movimento => (
+             {
+              name: movimento.move.name,
+              lv: movimento['version_group_details'].find(version => encontrarVersao(version)).level_learned_at
+             }
+         )
      )
 }))
 
-console.log(imprimir)
+console.dir(imprimir, {depth: null})
 /* attributes: {
     hp: encontrarStatus(umAtributoQualquer, 'hp')
     defesa: encontrarStatus(umAtributoQualquer, 'defense')
