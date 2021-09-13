de0da753beb6aa659b004e14990850ef06851bc7
Bianca Salino <biancadsalino@gmail.com>
2021-09-13 12:03:05 -0300

    retornando Tipo, ability e atributos

--- pokemonMapping.js
+++ pokemonMapping.js
@@ -1,18 +1,46 @@
+const { types } = require("../../pokemon/Pikachu/Pokemon Pikachu/Pikachu")
 const response = require("./api")
 
-const pokemon = response[0].moves.filter(movimento => movimento.version_group_details.filter(level => level.level_learned_at
-  && level.version_group.name == "red-blue").length).map(item_moves => ({
-    name: item_moves.move.name,
-    lv: item_moves.version_group_details[0].level_learned_at,
-  }))
+// const pokemon = response[0].moves.filter(movimento => movimento.version_group_details.filter(level => level.level_learned_at
+//   && level.version_group.name == "red-blue").length).map(item_moves => ({
+//     name: item_moves.move.name,
+//     lv: item_moves.version_group_details[0].level_learned_at,
+//   }))
 
-console.log(pokemon)
+// console.log(pokemon)
 
-function teste() {
-  const pokemon = response[0].moves.map(teste2 => teste2.version_group_details.map(teste3 => teste3.level_learned_at
-    && teste3.version_group.name == "red-blue"))
+// function teste() {
+//   const pokemon = response[0].moves.map(teste2 => teste2.version_group_details.map(teste3 => teste3.level_learned_at
+//     && teste3.version_group.name == "red-blue"))
 
-  return console.log(pokemon)
+//   return console.log(pokemon)
+// }
+
+// teste()
+
+
+const retornaTipo = types => {
+  return types.map(type => type.type.name)
 }
 
-teste()
+const retornaAbilities = abilities =>{
+  return abilities.map(habilidade => habilidade.ability.name)
+}
+
+const retornaAtributos = stats =>{
+  return stats.map(atributo => console.log(atributo.base_stat))
+}
+
+const pokemon = response.map(pkm => {
+  
+  return {
+
+    id: pkm.id,
+    name: pkm.name,
+    types: retornaTipo(pkm.types),
+    abilities: retornaAbilities(pkm.abilities),
+    attributes: retornaAtributos(pkm.stats)
+  }
+})
+
+console.log(pokemon)
