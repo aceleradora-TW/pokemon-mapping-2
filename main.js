19ee0f6540a92c58a73560b658fd1bed544b35d4
Jorel Martins <bearshamis@gmail.com>
2021-09-13 12:07:51 -0300

    feito

--- main.js
+++ main.js
@@ -1,66 +1,41 @@
 const response = require("./api")
 
-const capitalize = (string) => {
-    // se a palavra nao for válida retornará null
-    if (!isValid(string)) return null
-  
-    // caso seja válida, vai separar as palavras por espaço e criar um array com elas
-    let words = string.split(" ")
-    // com esse array, utilizo o map para conseguir transformar a primeira inicial de cada palavra em maiuscula
-    return words.map(
-      (word) => `${word[0].toUpperCase()}${word.substring(1)}`
-    ).join(" ") //ao final, uso join para juntar o array novamente em uma palavra apenas
-  }
-
-function obterNumeros(pokemon) {return parseInt(pokemon)}
-const formatNumber = number => parseFloat(number.toFixed(1))
 
 
-const buscarnome = (lista) => {
-  
-    const buscandoNome = lista.find((response) => response === 'name' )
-    return buscandoNome
-
-    // const inventory = [
-    //     {name: 'apples', quantity: 2},
-    //     {name: 'bananas', quantity: 0},
-    //     {name: 'cerejas', quantity: 5}
-    // ];
-    
-    // function isCherries(fruit) {
-    //     return fruit.name === 'cerejas';
-    // }
-    
-    // console.log(inventory.find(isCherries));
-    // // { name: 'cerejas', quantity: 5 }
-
-
-    // const spaces = "   "
-    // return `${spaces} Lv ${capitalize(name.forms.name)} - ${obterNumeros(id.id)}`
-    
-  }
-
-
-
-
-
-const imprimir = () =>{
-console.log(buscarnome(response));
-    
+const redBlue = (pokemon) => {
+  return pokemon["version_group"].name === "red-blue"
 }
 
-imprimir()
+const removeMachine = (pokemon) =>{
+    const version = pokemon["version_group_details"].find(version=>redBlue(version))
+    if (!version){
+      return false
+    } 
+    return version["move_learn_method"].name !== "machine" && version["move_learn_method"].name !== "tutor"
 
-// const pokemon = {
-//     id: Number,
-//     name: String,
-//     types: Array, // Array de string
-//     abilities: Array, // Array de string
-//     attributes: {
-//       hp: Number,
-//       attack: Number,
-//       specialAttack: Number,
-//       defense: Number,
-//       specialDefense: Number,
-//       speed: Number
-//     },
+}
+
+const pokemon = response.map(pkm => (
+  {
+    id: pkm.id,
+    name: pkm.name,
+    type : [pkm.types[0].type.name],
+    abilities : [pkm.abilities[0].ability.name,pkm.abilities[1].ability.name],
+    attributes :{
+        hp : pkm.stats[0].base_stat,
+        attack : pkm.stats[1].base_stat,
+        specialAttack : pkm.stats[3].base_stat, 
+        defense : pkm.stats[2].base_stat,
+        specialDefense : pkm.stats[4].base_stat,
+        speed : pkm.stats[5].base_stat 
+        },
+
+     moves : pkm.moves.filter(find => removeMachine(find)).map(maping=>({
+       name : maping.move.name,
+       lv : maping["version_group_details"].find(lvl=> redBlue(lvl)).level_learned_at
+     }))
+
+     
+  }))
+ 
+console.dir(pokemon,{depth: null});
