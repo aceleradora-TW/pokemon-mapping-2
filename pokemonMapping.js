c9878a2d0f27fe6c963c0b8ebdd33a86b36a4fb5
luiz Henrique <luizlhra24@gmail.com>
2021-09-13 10:28:59 -0300

    testes map e filter

--- pokemonMapping.js
+++ pokemonMapping.js
@@ -1,6 +1,6 @@
 const response = require("./api")
 
-const pokemon = response[0].moves.filter(movimento => movimento.version_group_details.filter(level => level.level_learned_at > 0
+const pokemon = response[0].moves.filter(movimento => movimento.version_group_details.filter(level => level.level_learned_at
   && level.version_group.name == "red-blue").length).map(item_moves => ({
     name: item_moves.move.name,
     lv: item_moves.version_group_details[0].level_learned_at,
@@ -8,11 +8,11 @@ const pokemon = response[0].moves.filter(movimento => movimento.version_group_de
 
 console.log(pokemon)
 
-// function teste() {
-//   const pokemon = response.map(rasp => rasp.moves.map(teste2 => teste2.version_group_details.map(teste3 => teste3.version_group.name == "red-blue")))
-//   return console.log(pokemon)
-// }
+function teste() {
+  const pokemon = response[0].moves.map(teste2 => teste2.version_group_details.map(teste3 => teste3.level_learned_at
+    && teste3.version_group.name == "red-blue"))
 
-// teste()
+  return console.log(pokemon)
+}
 
-//const pokemon = response[0].moves.map(movimento => movimento.version_group_details)
+teste()
