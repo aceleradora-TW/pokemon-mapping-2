0ea2beb618a3f32731d1506adfc3682d198b445f
luiz Henrique <luizlhra24@gmail.com>
2021-09-13 09:34:25 -0300

    teste

--- pokemonMapping.js
+++ pokemonMapping.js
@@ -1,6 +1,5 @@
 const response = require("./api")
 
-
 const pokemon = response[0].moves.filter(movimento => movimento.version_group_details.filter(level => level.level_learned_at > 0
   && level.version_group.name == "red-blue").length).map(item_moves => ({
     name: item_moves.move.name,
