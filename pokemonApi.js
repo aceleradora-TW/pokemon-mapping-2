9327a02276bd9242a7e9077ce8fe91a19f881103
patricia <patriciatersariol@gmail.com>
2021-09-14 10:16:05 -0300

    finalizacao do exercicio

--- pokemonApi.js
+++ pokemonApi.js
@@ -1,15 +1,5 @@
 const zeRoberto = require("./api")
 
-// response obj = forms obj = atributo "name" (tem nome do pokemon)
-// moves obj = version_group_details obj = version-group obj = atributo "name" red-blue
-// abilities obj = ability obj = atributo name
-// stats = hp, attack, defense, specialDefense, speed
-// types obj = type obj = name
-
-// poke.moves.filter(move => {
-// version_group_details.filter()
-// })
-
 const getTypes = (poke) =>
   poke.types.map((descobreTipos) => descobreTipos.type.name)
 
@@ -35,15 +25,22 @@ const getMoves = (poke) => {
     .filter(
       (move) =>
         move.version_group_details.find(
-          (version) => version.version_group.name === "red-blue"
+          (version) =>
+            version.version_group.name === "red-blue" &&
+            version.move_learn_method.name !== "machine" &&
+            version.move_learn_method.name !== "tutor"
         ) !== undefined
     )
-    .map((move) => console.log(move))
+    .map((move) => ({
+      move: move.move.name,
+      lv: move.version_group_details[0].level_learned_at
+    }))
 }
 
 const resultado = (poke) =>
   poke.map((poke) => {
     return {
+      id: poke.id,
       name: poke.name,
       types: getTypes(poke),
       abilities: getAbilities(poke),
@@ -53,29 +50,3 @@ const resultado = (poke) =>
   })
 
 console.dir(resultado(zeRoberto), { depth: null })
-
-// [
-//     {
-//       name: 'charmander',
-//       types: [ 'fire' ],
-//       abilities: [ 'blaze', 'solar-power' ],
-//       attributes: {
-//         hp: 39,
-//         attack: 52,
-//         specialAttack: 60,
-//         defense: 43,
-//         specialDefense: 50,
-//         speed: 65
-//       },
-//       moves: [
-//         { name: 'scratch', lv: 1 },
-//         { name: 'leer', lv: 15 },
-//         { name: 'growl', lv: 1 },
-//         { name: 'ember', lv: 9 },
-//         { name: 'flamethrower', lv: 38 },
-//         { name: 'fire-spin', lv: 46 },
-//         { name: 'rage', lv: 22 },
-//         { name: 'slash', lv: 30 }
-//       ]
-//     }
-//   ]
