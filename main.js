8419f267f6c2c3a360d04b53f1c6b2e6b699eba3
duda sadino <dudababy27@gmail.com>
2022-02-24 15:54:03 -0300

    lista dos moves feita

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let api = require("./api")
 function mapear(b) {
-    let teste = b.map(function (a) {
-        return a.moves
+    let teste = b.map(a => {
+        return a.moves.map(c => {
+            return c.move
+        })
     })
     return teste
     // console.log(map.type)
@@ -9,6 +11,26 @@ function mapear(b) {
     //     teste: map
 }
 console.log(mapear(api))
+const pokemon = {
+    id: Number,
+    name: String,
+    types: Array, // Array de string
+    abilities: Array, // Array de string
+    attributes: {
+        hp: Number,
+        attack: Number,
+        specialAttack: Number,
+        defense: Number,
+        specialDefense: Number,
+        speed: Number
+    },
+    moves: [
+        {
+            name: String,
+            lv: Number
+        }
+    ]
+}
 
 
     // let teste2 = b.filter(function (c) {
