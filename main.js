3579909f681b346a42f3c06c60bb76509d1b8aaa
Renato Souza <renatoosoouzaa26@gmail.com>
2022-02-25 11:52:13 -0300

    atribui informações sobre o pokemon

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
 let api = require("./api")
-function mapear(b) {
-    let teste = b.map(a => {
-        return a.moves.map(c => {
-            return c.move
+function pokemon(api) {
+    let id = api.map(numero => {
+        return numero.id
+    })
+    let nome = api.map(nome => {
+        return nome.name
+    })
+    let tipos = api.map(elemento => {
+        return elemento.types.map(type => {
+            return type.type.name
         })
     })
-    return teste
+    let habilidades = api.map(hab => {
+        return hab.abilities.map(abilit => {
+            return abilit.ability
+        })
+    })
+    let atributos = api.map(attrib => {
+        return attrib.stats.map(status => {
+            for (prop in status.stat.name) {
+                console.log(`status ${prop} = ${status.stat.name}`)
+            }
+            return status.stat.name
+        })
+    })
+    let movimentos = api.map(lista => {
+        return lista.moves.map(ataques => {
+            return ataques.move.name
+        })
+    })
+    return {
+        id: id,
+        name: nome,
+        type: tipos,
+        moves: movimentos,
+        attributes: atributos
+    }
     // console.log(map.type)
     // const pokemon = {
     //     teste: map
 }
-console.log(mapear(api))
-const pokemon = {
+console.log(pokemon(api))
+// function id (api){
+//     let id = api.map(numero => {
+//         return numero.id
+//     })
+//     return id
+// }
+// console.log(id(api))
+const pokEmon = {
     id: Number,
     name: String,
     types: Array, // Array de string
