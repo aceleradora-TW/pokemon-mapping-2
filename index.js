8e134eef72112c30cc6d34f8845306485e569e50
Julio Cesar <juliocesardantss@gmail.com>
2022-05-10 13:42:03 -0300

    exercicio de moves

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const bancoPokApi = require('./api')
 
-const pokemonType =  tipos => {
-        return tipos.map(tipo => tipo.type.name)
-    }
-    
-const pokemonAbilities =  abilities => {
+const pokemonType = tipos => {
+    return tipos.map(tipo => tipo.type.name)
+}
+
+const pokemonAbilities = abilities => {
     return abilities.map(abilitie => abilitie.ability.name)
 }
 
@@ -14,43 +14,43 @@ const mapeandoAtributos = (status, atributos) => {
 
 }
 
-const mapeandoMoves = moves => { 
+const mapeandoMoves = moves => {
     return moves.moves.filter(move =>
         move.version_group_details.find(name =>
             name.version_group.name === "red-blue" && name.move_learn_method.name !== "machine" && name.move_learn_method.name !== "tutor"))
-        .map(move =>{
-            name: move.move.name,
-
-    })
+        .map(move => ({
+            name: move.move.name, lv: move.move.lv            
+        }))
 }
 
-function mapeandoPokemon(pokemons){
+function mapeandoPokemon(pokemons) {
     const resultado = pokemons.map(pokemon => {
-        return{
-    id: pokemon.id,
-    name: pokemon.name,
-    types: pokemonType(pokemon.types),
-    abilities: pokemonAbilities(pokemon.abilities),
-    attributes: {
-      hp: mapeandoAtributos(pokemon,"hp"),
-      attack: mapeandoAtributos(pokemon,"attack"),
-      specialAttack: mapeandoAtributos(pokemon,"special-attack"),
-      defense: mapeandoAtributos(pokemon,"defense"),
-      specialDefense: mapeandoAtributos(pokemon,"special-defense"),
-      speed: mapeandoAtributos(pokemon,"speed")
-    },
-    moves2: mapeandoMoves(pokemon),
-    moves: [
-        { name: 'scratch', lv: 1 },
-        { name: 'leer', lv: 15 },
-        { name: 'growl', lv: 1 },
-        { name: 'ember', lv: 9 },
-        { name: 'flamethrower', lv: 38 },
-        { name: 'fire-spin', lv: 46 },
-        { name: 'rage', lv: 22 },
-        { name: 'slash', lv: 30 }
-      ]
-    }})
+        return {
+            id: pokemon.id,
+            name: pokemon.name,
+            types: pokemonType(pokemon.types),
+            abilities: pokemonAbilities(pokemon.abilities),
+            attributes: {
+                hp: mapeandoAtributos(pokemon, "hp"),
+                attack: mapeandoAtributos(pokemon, "attack"),
+                specialAttack: mapeandoAtributos(pokemon, "special-attack"),
+                defense: mapeandoAtributos(pokemon, "defense"),
+                specialDefense: mapeandoAtributos(pokemon, "special-defense"),
+                speed: mapeandoAtributos(pokemon, "speed")
+            },
+            moves2: mapeandoMoves(pokemon),
+            moves: [
+                { name: 'scratch', lv: 1 },
+                { name: 'leer', lv: 15 },
+                { name: 'growl', lv: 1 },
+                { name: 'ember', lv: 9 },
+                { name: 'flamethrower', lv: 38 },
+                { name: 'fire-spin', lv: 46 },
+                { name: 'rage', lv: 22 },
+                { name: 'slash', lv: 30 }
+            ]
+        }
+    })
     return resultado
 }
-console.dir(mapeandoPokemon(bancoPokApi),{depth: 1000})
+console.dir(mapeandoPokemon(bancoPokApi), { depth: 1000 })
