8fe6348641e63c615bb71da2e177e9f8df1b61fe
cintia <cintia.silvadeooliveira@gmail.com>
2022-05-13 10:14:23 -0300

    exercicio em andamento filtrando o red-blue

--- pokemon.js
+++ pokemon.js
@@ -3,17 +3,18 @@ const response = require('./api')
 //percorrer o array com o map
 //criar um objeto filtrando os moves red-blue
 //ordenar os moves em ordem crescente
-const filtrarPokemonMovesName = (version_group) =>{
-    return version_group.filter(version_group => version_group.name === red-blue)
+const filtrarPokemonMovesName = (version_group_details) =>{
+    const version = version_group_details.map( details => details.version_group)
+    return version.filter(version_group=> version_group.name === "red-blue")
 }
 
 const filtrarPokemonMoves = (moves) =>{
-    return moves.filter(move => filtrarPokemonMovesName(move.version_group))
+    return moves.map(move => filtrarPokemonMovesName(move.version_group_details))
 }
 
 const pokemon = (listaPkm) => {
-    const pkmMoves = listaPkm.filter(pkm => filtrarPokemonMoves(pkm.moves))
-    console.log(pkmMoves)
+    const pkmMoves = listaPkm.map(pkm => filtrarPokemonMoves(pkm.moves))
+    console.dir(pkmMoves, {depth:null})
 }
 
-console.dir(pokemon(response, {depth:null}))
+pokemon(response)
