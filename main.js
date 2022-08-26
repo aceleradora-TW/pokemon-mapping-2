f1b4a16f5f78a3adb417546fdfc931ef0ef33946
Enzo Soares <soaressenzo@gmail.com>
2022-08-26 11:16:02 -0300

    implementando a funcao getMoveName

--- main.js
+++ main.js
@@ -28,36 +28,54 @@ const updatePokemon = (pos, obj) => {
   for (let ability of response[pos].abilities) {
     obj.abilities.push(ability.ability.name);
   }
-  obj.attributes.attack = getStatus(pos,'attack');
-  obj.attributes.defense = getStatus(pos,'defense');
-  obj.attributes.hp = getStatus(pos,'hp');
-  obj.attributes.specialAttack = getStatus(pos,'special-attack');
-  obj.attributes.specialDefense = getStatus(pos,'special-defense');
-  obj.attributes.speed = getStatus(pos,'speed');
+  obj.attributes.attack = getStatus(pos, "attack");
+  obj.attributes.defense = getStatus(pos, "defense");
+  obj.attributes.hp = getStatus(pos, "hp");
+  obj.attributes.specialAttack = getStatus(pos, "special-attack");
+  obj.attributes.specialDefense = getStatus(pos, "special-defense");
+  obj.attributes.speed = getStatus(pos, "speed");
 };
-const getStatus = (pos,status) => {
-    const resultStatus = response[pos].stats.find((type=> type.stat.name == status));
-    return resultStatus.base_stat;
+const getStatus = (pos, status) => {
+  const resultStatus = response[pos].stats.find(
+    (type) => type.stat.name == status
+  );
+  return resultStatus.base_stat;
 };
-const getMove = (pos)=>{
-    const resultMoves = response[pos].moves.map(
-        (move)=>{
-            move.version_group_details.filter(
-                (detail)=>{
-                    (detail.version_group.name == 'red-blue' &&
-                    detail.move_learn_method.name != 'machine' &&
-                    detail.move_learn_method.name != 'tutor')     
-                }
-            ).map((detail)=>{
-                return{ 
-                    name: detail.move.name,
-                    lv: detail.level_learned_at
-                }
-            });
+const getMoveLevel = (pos) => {
+  let level = [];
+  const resultMoves = response[pos].moves.map(
+    (move) => {
+      return move.version_group_details.filter((detail)=>{
+        if(detail.version_group.name == 'red-blue' && detail.level_learned_at>0){
+          return detail.level_learned_at
         }
-    );
-    console.log(resultMoves);
-}
-getMove(0);
+      })
+    }
+  );
+  resultMoves.forEach((moves)=>{
+    moves.forEach((move)=>{
+      if(move){
+        level.push(move.level_learned_at)
+      }
+    })
+  });
+  console.log(level)
+  return level;
+};
+// const getMoveName = (pos)=>{
+//   let name =[];
+//   const level = getMoveLevel();
+//   const resultMovesNames = response[pos].moves.map(
+//     (move) => {
+//       return move.version_group_details.filter((detail)=>{
+//         if(detail.version_group.name == 'red-blue' && detail.level_learned_at>0){
+//           return move.move.name
+//         }
+//       })
+//     }
+//   );
+//   console.log(resultMovesNames);
+// }
+getMoveLevel(0);
 // updatePokemon(0, pokemon);
 // console.log(pokemon.name, pokemon.attributes.speed);
