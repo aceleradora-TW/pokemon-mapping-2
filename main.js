bfceeb096b3f9800e69187cecf66ed47ef9f098a
Vanessa <vanessa.almirante@hotmail.com>
2022-09-16 11:45:02 -0300

    primeiro commit

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const response = require("./api")
 
-const adapterAbilities = (abilities) => {
-  return abilities.map(ab => ab.ability.name)
+const adapterElements = (element, attribute) => {
+  return element.map( elem => elem[attribute].name)
+}
+
+const adapterAttributes = (attribute) => {
+  const attributes = attribute.map( elem => elem.base_stat);
+  return {
+      hp: attributes[0],
+      attack: attributes[1],
+      specialAttack: attributes[2],
+      defense: attributes[3],
+      specialDefense: attributes[4],
+      speed: attributes[5]
+  }
 }
 
 const adapterPokemon = (response) => {
@@ -9,11 +21,13 @@ const adapterPokemon = (response) => {
     return {
       name: pokemon.name,
       id: pokemon.id,
-      abilities: adapterAbilities(pokemon.abilities)
-    }
-  })
-}
+      types: adapterElements(pokemon.types, "type"),
+      abilities: adapterElements(pokemon.abilities, "ability"),
+      attributes:adapterAttributes(pokemon.stats),
+     }
+    })
+  }
+
 
 const pokemon = adapterPokemon(response)
 console.dir(pokemon, { depth: 999 })
-console.log()
