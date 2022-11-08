7c0f740c12dab7675029f47b57a68aa94dcac373
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-11-08 14:20:46 -0300

    Atualizando exercício

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
-const response = require("./api")
-
-const adapterAbilities = (abilities) => {
-  return abilities.map(ab => ab.ability.name)
+const response = require("./api");
+/*
+Apenas os moves que são da versão red-blue devem ser retornados para o objeto.
+Ordenar os moves em ordem crescente
+Não devem ser filtrados moves que tenha move_learn_method.name igual a machine ou tutor
+*/
+const adaptarHabilidades = (habilidade) => {
+ let teste;
+  habilidade.map((valor)=>{
+ valor.abilities.map((valor2)=>{
+  teste = valor2.ability.name
+ // console.log(valor2.ability.name)
+ })
+ })
+ 
 }
+adaptarHabilidades(response)
 
-const adapterPokemon = (response) => {
-  return response.map(pokemon => {
-    return {
-      name: pokemon.name,
-      id: pokemon.id,
-      abilities: adapterAbilities(pokemon.abilities)
-    }
-  })
-}
 
-const pokemon = adapterPokemon(response)
-console.dir(pokemon, { depth: 999 })
+const pkm = {
+  id: Number,
+  name: String,
+  types: [], // Array de string
+  abilities: [], // Array de string
+  attributes: {
+    hp: Number,
+    attack: Number,
+    specialAttack: Number,
+    defense: Number,
+    specialDefense: Number,
+    speed: Number,
+  },
+  moves: [
+    {
+      name: String,
+      lv: Number,
+    },
+  ],
+};
+
+//const pokemon = adapterPokemon(response);
+//console.dir(pokemon, { depth: 999 });
