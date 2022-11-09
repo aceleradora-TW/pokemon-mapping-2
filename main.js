b1eda8bde656476b65822811504c23af391dda74
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-11-09 14:15:00 -0300

    repassando exercícios

--- main.js
+++ main.js
@@ -3,19 +3,32 @@ const response = require("./api");
 Apenas os moves que são da versão red-blue devem ser retornados para o objeto.
 Ordenar os moves em ordem crescente
 Não devem ser filtrados moves que tenha move_learn_method.name igual a machine ou tutor
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
+
+
+
 */
 const adaptarHabilidades = (habilidade) => {
- let teste;
-  habilidade.map((valor)=>{
- valor.abilities.map((valor2)=>{
-  teste = valor2.ability.name
- // console.log(valor2.ability.name)
- })
- })
- 
-}
-adaptarHabilidades(response)
+  let poke = [];
+  poke.push(
+    habilidade.map((valor) => {
+      valor.abilities.map((valor2) => {
+        //if( ){
 
+        //}
+
+        return valor2.ability.name;
+      });
+    })
+  );
+  console.log(poke);
+};
+adaptarHabilidades(response);
 
 const pkm = {
   id: Number,
