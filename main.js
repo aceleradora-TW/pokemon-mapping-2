378988106ab26ece2fac45a4e1acc57908dc04d1
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-11-10 11:46:34 -0300

    Exercício quase finalizado

--- main.js
+++ main.js
@@ -10,46 +10,61 @@ const result = words.filter(word => word.length > 6);
 console.log(result);
 // expected output: Array ["exuberant", "destruction", "present"]
 
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
+const result = words.filter(word => word.length > 6);
 
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element > 10);
 */
-const adaptarHabilidades = (habilidade) => {
-  let poke = [];
-  poke.push(
-    habilidade.map((valor) => {
-      valor.abilities.map((valor2) => {
-        //if( ){
 
-        //}
-
-        return valor2.ability.name;
-      });
-    })
-  );
-  console.log(poke);
+const filtraAtributos = (atributos, statName) => {
+  return atributos.find((atributo) => atributo.stat.name === statName);
 };
-adaptarHabilidades(response);
 
-const pkm = {
-  id: Number,
-  name: String,
-  types: [], // Array de string
-  abilities: [], // Array de string
-  attributes: {
-    hp: Number,
-    attack: Number,
-    specialAttack: Number,
-    defense: Number,
-    specialDefense: Number,
-    speed: Number,
-  },
-  moves: [
-    {
-      name: String,
-      lv: Number,
-    },
-  ],
+const adptarTipos = (tipos) => {
+  return tipos.map((tipo) => {
+    return tipo.type.name;
+  });
 };
+
+const adaptarHabilidades = (habilidades) => {
+  return habilidades.map((habilidade) => {
+    return habilidade.ability.name;
+  });
+};
+const adpatarPkm = (pokemon) => {
+  return pokemon.map((pkmObj) => {
+    console.log(filtraAtributos(pkmObj.stats));
+    return {
+      id: pkmObj.id,
+      name: pkmObj.name,
+      types: adptarTipos(pkmObj.types), // Array de string
+      abilities: adaptarHabilidades(pkmObj.abilities), // Array de string
+      attributes: {
+        hp: filtraAtributos(pkmObj.stats, "hp").base_stat,
+        attack: filtraAtributos(pkmObj.stats, "attack").base_stat,
+        specialAttack: filtraAtributos(pkmObj.stats, "special-attack")
+          .base_stat,
+        defense: filtraAtributos(pkmObj.stats, "defense").base_stat,
+        specialDefense: filtraAtributos(pkmObj.stats, "special-defense")
+          .base_stat,
+        speed: filtraAtributos(pkmObj.stats, "speed").base_stat,
+      },
+      moves: [
+        {
+          name: String,
+          lv: Number,
+        },
+      ],
+    };
+  });
+};
+console.dir(adpatarPkm(response), { depth: 9999 });
+//const resultPokemon = response.map((pkmObj) => {
+
+//});
 
 //const pokemon = adapterPokemon(response);
 //console.dir(pokemon, { depth: 999 });
