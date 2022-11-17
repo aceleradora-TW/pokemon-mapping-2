c5573da2c3bb5c3398ee069485f025be294aee88
Gabriela_gomes3108 <gabrieladepaula8568@gmail.com>
2022-11-17 13:22:03 -0300

    Finalizando exercicios

--- main.js
+++ main.js
@@ -1,74 +1,53 @@
 const response = require("./api");
 
-const filtraAtributos = (atributos, statName) =>
-  atributos.find((atributo) => atributo.stat.name === statName);
-
 const adptarTipos = (tipos) => tipos.map((tipo) => tipo.type.name);
 
-const adaptarHabilidades = (habilidades) =>
-  habilidades.map((habilidade) => habilidade.ability.name);
+const adaptarHabilidades = (habilidades) => habilidades
+  .map((habilidade) => habilidade.ability.name);
+
+const filtraAtributos = (atributos, statName) => atributos
+  .find((atributo) => atributo.stat.name === statName).base_stat
 
 const adaptarMoves = (movimentos) => {
   return movimentos
-    .map((move) =>
-      move.version_group_details
-        .filter((detailsMove) => {
-          return (
-            detailsMove.version_group.name === "red-blue" &&
-            detailsMove.move_learn_method.name !== "machine" &&
-            detailsMove.move_learn_method.name !== "tutor"
-          );
-        })
-        .map((nameRedBlue) => {
-          return {
-            name: move.move.name,
-            level: nameRedBlue.level_learned_at,
-          };
-        })
+    .map((move) => move.version_group_details
+      .filter((detailsMove) => {
+        return (
+          detailsMove.version_group.name === "red-blue" &&
+          detailsMove.move_learn_method.name !== "machine" &&
+          detailsMove.move_learn_method.name !== "tutor"
+        );
+      })
+      .map((nameRedBlue) => {
+        return {
+          name: move.move.name,
+          level: nameRedBlue.level_learned_at,
+        };
+      })
     )
-    .map((finashName) => {
-      finashName.find((test)=>{
-         console.log(test)
-     })
- 
-    });
+    .filter((test) => test[0])
 
-  //.filter((filtrarValueOn) => filtrarValueOn === "") console.log(filtrarValueOn)
-};
+    .map((arrayDeObjetos) => arrayDeObjetos[0])
 
-// movimento.filter(filtrarMovi => filtrarMovi == version_group)
+    .sort((anterior, proximo) => anterior.level - proximo.level)
+}
+
 const adpatarPkm = (pokemon) =>
   pokemon.map((pkmObj) => {
-    // console.log(adaptarMoves(pkmObj.moves));
     return {
       id: pkmObj.id,
       name: pkmObj.name,
       types: adptarTipos(pkmObj.types),
       abilities: adaptarHabilidades(pkmObj.abilities),
       attributes: {
-        hp: filtraAtributos(pkmObj.stats, "hp").base_stat,
-        attack: filtraAtributos(pkmObj.stats, "attack").base_stat,
-        specialAttack: filtraAtributos(pkmObj.stats, "special-attack")
-          .base_stat,
-        defense: filtraAtributos(pkmObj.stats, "defense").base_stat,
-        specialDefense: filtraAtributos(pkmObj.stats, "special-defense")
-          .base_stat,
-        speed: filtraAtributos(pkmObj.stats, "speed").base_stat,
+        hp: filtraAtributos(pkmObj.stats, "hp"),
+        attack: filtraAtributos(pkmObj.stats, "attack"),
+        specialAttack: filtraAtributos(pkmObj.stats, "special-attack"),
+        defense: filtraAtributos(pkmObj.stats, "defense"),
+        specialDefense: filtraAtributos(pkmObj.stats, "special-defense"),
+        speed: filtraAtributos(pkmObj.stats, "speed"),
       },
       moves: adaptarMoves(pkmObj.moves),
     };
   });
-//const adaptarMoves = (movimentos) => movimentos.map((movimento) => movimento.moves)
-//adaptarMoves(response.moves)
 console.dir(adpatarPkm(response), { depth: 9999 });
-
-/*
-
-const adaptarMoves = (movimentos) => {
-  movimentos.map((move) => {
-    move.version_group_details.map((valor2)=>{
-      valor2
-    });
-  });
-
-*/
